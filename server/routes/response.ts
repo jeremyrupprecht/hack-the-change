1df1b60f691bab76f2c1051ea39129c67f50ad05
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-19 16:31:08 -0700

    I'm on main

--- server/routes/response.ts
+++ server/routes/response.ts
@@ -11,4 +11,54 @@ responseRouter.use((req: Request, res: Response, next: () => void) => {
   next()
 })
 
+
+// From FastAPI Microservice
+responseRouter.post('/', async (req: Request, res: Response) => {
+  try {
+      const { pollId, userId, responseString, description } = req.body;
+      // description must be parsed
+      const { feeling, sentiment, method, reason } = description;
+      // model PollResponse {
+      //   id        String   @id @default(uuid())
+      //   response  String
+      //   feeling   String
+      //   sentiment String
+      //   method    String
+      //   reason    String
+      //   poll      Poll     @relation(fields: [pollId], references: [id])
+      //   pollId    String
+      //   user      User     @relation(fields: [userId], references: [id])
+      //   userId    String
+      //   createdAt DateTime @default(now())
+      //   updatedAt DateTime @updatedAt
+      // }
+      
+      const newResponse = await prisma.response.create({
+          data: {
+              response: responseString,
+              pollId,
+              userId,
+              feeling,
+              sentiment,
+              method,
+              reason,
+          }
+      });
+
+      console.log(`New Response: `, newResponse);
+
+
+      return res.json({
+          success: true,
+          data: newResponse
+      });
+
+  } catch (error) {
+      return res.json({
+          success: false,
+          message: error
+      });
+  }
+});
+
 module.exports = responseRouter
